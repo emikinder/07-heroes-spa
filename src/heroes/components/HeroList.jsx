7e841bdc04db9c6bcc0cb24919a53c12dfc34fb7
Emi Kinderknecht <emikinderk@gmail.com>
2022-10-13 23:46:13 -0300

    added search input

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,8 +1,9 @@
 import { HeroCard } from './';
 import { getHeroesByPublisher } from '../helpers';
+import { useMemo } from 'react';
 
 export const HeroList = ({ publisher }) => {
-	const heroes = getHeroesByPublisher(publisher);
+	const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
 	return (
 		<div className='row row-cols-1 row-cols-md-3 g-3'>
--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,54 @@
+import { useMemo } from 'react';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import { getHeroById } from '../helpers';
 
 export const HeroPage = () => {
-	return <div>Hero</div>;
+	const { id, ...rest } = useParams();
+	const hero = useMemo(() => getHeroById(id), [id]);
+
+	const navigate = useNavigate();
+	const onNavigateBack = () => {
+		const path = hero.publisher === 'Marvel Comics' ? '/marvel' : '/dc';
+		navigate(`${path}`, { replace: true });
+	};
+
+	// in case that hero is undefined
+	if (!hero) {
+		return <Navigate to='/marvel' />;
+	}
+
+	return (
+		<div className='row mt-5'>
+			<div className='col-4 animate__animated animate__fadeInLeft'>
+				<img
+					src={`/assets/heroes/${id}.jpg`}
+					alt={hero.superhero}
+					className='img-thumbnail'
+				/>
+			</div>
+			<div className='col-8'>
+				<h3>{hero.superhero}</h3>
+				<ul className='list-group list-group-flush'>
+					<li className='list-group-item'>
+						<b>Alter ego: </b> {hero.alter_ego}
+					</li>
+					<li className='list-group-item'>
+						<b>Publisher: </b> {hero.publisher}
+					</li>
+					<li className='list-group-item'>
+						<b>First appearance: </b> {hero.first_appearance}
+					</li>
+				</ul>
+				<h5 className='mt-3'>Characters</h5>
+				<p>{hero.characters}</p>
+
+				<button
+					className='btn btn-outline-secondary'
+					onClick={onNavigateBack}
+				>
+					Back
+				</button>
+			</div>
+		</div>
+	);
 };
